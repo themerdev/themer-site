6a8814949fbf976ad0c829331456972ebcfc0076
Matt Swensen <swensen.matthew@gmail.com>
2017-07-26 13:05:29 -0600

    Moving generated previews of color sets from themer-previews repo

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+mkdir -p dist
+
+# Main site files
+cp apps.jpg dist/
+cp basicGrid.min.css dist/
+cp cli.jpg dist/
+cp favicon.png dist/
+cp index.css dist/
+cp index.html dist/
+cp intro.mp4 dist/
+cp logo.png dist/
+cp themes.jpg dist/
+cp wallpaper.jpg dist/
+
+# Generate preview files
+themer -c themer-colors-default -t scripts/themer-previews.js -o tmp
+themer -c themer-colors-night-sky -t scripts/themer-previews.js -o tmp
+themer -c themer-colors-one -t scripts/themer-previews.js -o tmp
+themer -c themer-colors-polar-ice -t scripts/themer-previews.js -o tmp
+mv tmp/themer-previews.js dist/color-set-previews
+rmdir tmp
